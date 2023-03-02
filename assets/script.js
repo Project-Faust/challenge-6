2e6e1771c6d9a71b21df868c9a15d0dda3b78f25
Project Faust <pentecost.cody.lane@gmail.com>
2023-03-01 21:57:38 -0500

    added bootstrap styling; added event handler for search form

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,29 @@
+var searchBtn = $('search-button');
 
+// event handler for search button
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+    var searchInputVal = $('')
+};
+
+var showCurrentWeather = function (data) {
+
+};
+
+var getWeather = function (city) {
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + citySearch + '896dd63ca789f53ced8a876118b951a4')
+};
+
+var citySearch = function () {
+
+};
+
+
+
+var displayExtended = function () {
+
+};
+
+// weather api key
+// 896dd63ca789f53ced8a876118b951a4
