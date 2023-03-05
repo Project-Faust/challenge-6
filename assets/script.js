f21d49956e444d1680f2d494c3091cbca8f8d73e
Lane Pentecost <pentecost.cody.lane@gmail.com>
2023-03-04 21:00:31 -0500

    updated weather api using lat/lon to retrieve data

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,40 @@
 const searchBtn = document.getElementById('search-button');
 const searchForm = document.getElementById('search-form');
-console.log(searchForm);
-console.log(searchForm.value);
+// console.log(searchForm);
+// console.log(searchForm.value);
 
 // event handler for search button
 searchBtn.addEventListener('click', (event) => {
     event.preventDefault();
     console.log('click')
     var userCity = searchForm.value;
-    var geoCity = function () {
-
-    };
     fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&units=standard&appid=896dd63ca789f53ced8a876118b951a4')
         .then(function (response) {
             if (!response.ok) {
                 throw response.json;
             };
 
-            console.log(response);
+            // console.log(response);
             return response.json();
         })
         .then(function (data) {
-            console.log(data[0]);
+            console.log(data);
             var lat = data[0].lat;
-            console.log(lat);
+            // console.log(lat);
             var lon = data[0].lon;
-            console.log(lon);
-            return fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=896dd63ca789f53ced8a876118b951a4');
+            // console.log(lon);
+            fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=896dd63ca789f53ced8a876118b951a4')
+                .then(response => response.json())
+                .then(data => {
+                console.log(data);
+                var forecastData = data.list.splice(0,5);
+                console.log(forecastData);
+                // date in text content
+                var currentDate = new Date();
+                // var currentCity = data.city.name
+            })
         })
 });
-
-var showCurrentWeather = function (data) {
-
-};
-
-
-var citySearch = function () {
-
-};
-
-var displayExtended = function () {
-
-};
 
 // weather api key
 // 896dd63ca789f53ced8a876118b951a4
