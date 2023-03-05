8a2ce2579cdcafc5251a9d7b775025e55ef7eb18
Lane Pentecost <pentecost.cody.lane@gmail.com>
2023-03-04 20:08:37 -0500

    modified js link in html; added api fetch for lat/lon coordinates

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,43 @@
-var searchBtn = $('search-button');
+const searchBtn = document.getElementById('search-button');
+const searchForm = document.getElementById('search-form');
+console.log(searchForm);
+console.log(searchForm.value);
 
 // event handler for search button
-function handleSearchFormSubmit(event) {
+searchBtn.addEventListener('click', (event) => {
     event.preventDefault();
+    console.log('click')
+    var userCity = searchForm.value;
+    var geoCity = function () {
 
-    var searchInputVal = $('')
-};
+    };
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + userCity + '&units=standard&appid=896dd63ca789f53ced8a876118b951a4')
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json;
+            };
+
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data[0]);
+            var lat = data[0].lat;
+            console.log(lat);
+            var lon = data[0].lon;
+            console.log(lon);
+            return fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=896dd63ca789f53ced8a876118b951a4');
+        })
+});
 
 var showCurrentWeather = function (data) {
 
 };
 
-var getWeather = function (city) {
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + citySearch + '896dd63ca789f53ced8a876118b951a4')
-};
 
 var citySearch = function () {
 
 };
-
-
 
 var displayExtended = function () {
 
